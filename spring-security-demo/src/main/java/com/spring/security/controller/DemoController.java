ab9f5baea3f0af27d213834f8f947de1113a2263
soumyadip007 <soumyadip.note@gmail.com>
2019-05-27 00:30:16 +0530

    DemoController & home.jsp added (no XML config)

---
+++ spring-security-demo/src/main/java/com/spring/security/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showHome()
+	{
+		return "home";
+	}
+}
