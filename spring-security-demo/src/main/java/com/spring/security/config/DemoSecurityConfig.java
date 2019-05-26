7eed24814c3873eb77c621c8582374aa937ccb14
soumyadip007 <soumyadip.note@gmail.com>
2019-05-27 01:30:55 +0530

    DemoSecurityConfig Bug Fixed

--- spring-security-demo/src/main/java/com/spring/security/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/spring/security/config/DemoSecurityConfig.java
@@ -2,24 +2,27 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
-@Configuration			//Replacement of web.xml
-@EnableWebMvc			//Replacement of spring-mvc-demo.xml
+@Configuration          //Replacement of web.xml
+@EnableWebSecurity		//Replacement of spring-mvc-demo.xml
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
 		
-	//add out users for in memory authentication
-		
-		UserBuilder user=User.withDefaultPasswordEncoder();
+		UserBuilder users = User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication()
-			.withUser(user.username("john").password("123").roles("dev"));
-	}	
+			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("mary").password("test123").roles("MANAGER"))
+			.withUser(users.username("susan").password("test123").roles("ADMIN"));
+	}
+	
 }
