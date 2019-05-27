ac94c61f0f4b068256077ab1705c2f3c42488725
soumyadip007 <soumyadip.note@gmail.com>
2019-05-27 22:46:41 +0530

    Patch failed

--- spring-security-demo/src/main/java/com/spring/security/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/spring/security/config/DemoSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration          //Replacement of web.xml
 @EnableWebSecurity		//Replacement of spring-mvc-demo.xml
@@ -33,7 +32,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	
 		
-	/*	http.authorizeRequests()
+		http.authorizeRequests()
 		.antMatchers("/","/leaders").hasAuthority("ADMIN").anyRequest().authenticated()
 			.and()
 			.formLogin()
@@ -41,16 +40,10 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginProcessingUrl("/authenticateTheUser")
 			.permitAll()
 			.and()
-			.logout().permitAll();*/
+			.logout().permitAll();
 		
 		
-		http.
-		authorizeRequests()
-			.antMatchers("/**").hasAuthority("ADMIN").anyRequest()
-			.authenticated().and().formLogin()
-			.loginPage("/showMyLoginPage").permitAll()
-			.and().logout()
-			.logoutSuccessUrl("/login");
+		
 		//logoutSuccessUrl("/customLogout")
 	}
 	
